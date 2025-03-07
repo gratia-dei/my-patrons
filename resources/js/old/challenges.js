23f553b4fd50b7e456668d101b8e868fcaf10c46
Andrzej Precz <andrzej.precz@gmail.com>
2025-03-07 10:58:13 +0100

    refactoring JS: move old achievements functions to achievements.js

--- resources/js/old/challenges.js
+++ resources/js/old/challenges.js
@@ -44,13 +44,6 @@ requirejs(
 
   //-------------
 
-  const ACHIEVEMENTS_GENERAL_ALL = 'lang-achievements-section-general-number-of-challenges-started';
-  const ACHIEVEMENTS_GENERAL_DONE_COMPLETELY = 'lang-achievements-section-general-done-completely';
-  const ACHIEVEMENTS_GENERAL_DONE = 'lang-achievements-section-general-done-without-some-optional-steps';
-  const ACHIEVEMENTS_GENERAL_WAITING = 'lang-achievements-section-general-waiting-to-be-completed';
-  const ACHIEVEMENTS_GENERAL_ABORTED = 'lang-achievements-section-general-aborted';
-  const ACHIEVEMENTS_GENERAL_TODO = 'lang-achievements-section-general-with-todo-status';
-
   const SELECT_NAME = '...';
   const SELECT_SEPARATOR = '----------';
 
@@ -166,7 +159,6 @@ requirejs(
   const CHALLENGE_SUCCESS_STATUS_ICON_DONE_ELEMENT_ID_PREFIX = 'challenge-success-status-icon-done-';
   const CHALLENGE_SUCCESS_STATUS_ICON_DONE_WITHOUT_ANY_OPTIONAL_STEPS_ELEMENT_ID_PREFIX = 'challenge-success-status-icon-done-without-any-optional-steps-';
   const RANDOM_BIBLE_CHAPTERS_BUTTON_ELEMENT_ID = 'random-bible-chapter';
-  const ACHIEVEMENTS_GENERAL_TABLE_ELEMENT_ID = 'achievements-general-table';
   const CHALLENGES_SHOW_FOR_NO_ROWS_CLASS_ID = 'challenges-show-for-no-rows';
   const CHALLENGES_SHOW_FOR_ANY_ROWS_CLASS_ID = 'challenges-show-for-any-rows';
 
@@ -4166,119 +4158,6 @@ requirejs(
   }
 
   build();
-
-
-
-  //////////////////////// functions to migrate
-
-
-
-
-
-
-
-  //achievements
-  function resetAchievements(fileData) {
-    const data = {};
-
-    let rowId = 0;
-    for (const challenge of fileData[DATA_FIELD_CHALLENGES] ?? []) {
-      rowId++;
-
-      const successStatus = getChallengeSuccessStatus(rowId);
-      const checklistSteps = challenge[DATA_FIELD_CHECKLIST] ?? {};
-
-      //general
-      data[ACHIEVEMENTS_GENERAL_ALL] = (data[ACHIEVEMENTS_GENERAL_ALL] ?? 0) + 1;
-      switch (successStatus) {
-        case CHALLENGE_SUCCESS_STATUS_DONE:
-          let index = ACHIEVEMENTS_GENERAL_DONE_COMPLETELY;
-          for (const statusInData of Object.values(checklistSteps)) {
-            if (statusInData === CHALLENGE_SUCCESS_STATUS_IN_DATA_WAITING) {
-              index = ACHIEVEMENTS_GENERAL_DONE;
-              break;
-            }
-          }
-          data[index] = (data[index] ?? 0) + 1;
-          break;
-
-        case CHALLENGE_SUCCESS_STATUS_WAITING:
-          data[ACHIEVEMENTS_GENERAL_WAITING] = (data[ACHIEVEMENTS_GENERAL_WAITING] ?? 0) + 1;
-          break;
-
-        case CHALLENGE_SUCCESS_STATUS_ABORTED:
-          data[ACHIEVEMENTS_GENERAL_ABORTED] = (data[ACHIEVEMENTS_GENERAL_ABORTED] ?? 0) + 1;
-          break;
-
-        case CHALLENGE_SUCCESS_STATUS_TODO:
-          data[ACHIEVEMENTS_GENERAL_TODO] = (data[ACHIEVEMENTS_GENERAL_TODO] ?? 0) + 1;
-          break;
-      }
-    }
-
-    //general
-    const labels = {
-      [ACHIEVEMENTS_GENERAL_ALL]: true,
-      [ACHIEVEMENTS_GENERAL_DONE_COMPLETELY]: false,
-      [ACHIEVEMENTS_GENERAL_DONE]: false,
-      [ACHIEVEMENTS_GENERAL_WAITING]: false,
-      [ACHIEVEMENTS_GENERAL_ABORTED]: false,
-      [ACHIEVEMENTS_GENERAL_TODO]: false
-    };
-    const showCountZeroRows = false;
-    createAchievementCounterTable(data, ACHIEVEMENTS_GENERAL_TABLE_ELEMENT_ID, labels, data[ACHIEVEMENTS_GENERAL_ALL] ?? 0, showCountZeroRows);
-  }
-
-  function createAchievementCounterTable(data, tableElementId, labels, houndredPercentCount = 0, showCountZeroRows = true) {
-    const tableElement = document.getElementById(tableElementId);
-    tableElement.innerHTML = '';
-
-    let isFirstRow = true;
-    for (const label of Object.keys(labels)) {
-      const isUpperCase = labels[label] ?? false;
-      const count = data[label] ?? 0;
-      if (!showCountZeroRows && count === 0) {
-        continue;
-      }
-
-      const row = document.createElement('tr');
-
-      const nameCell = document.createElement('td');
-      const name = uLanguage.getTranslation(label, isUpperCase);
-      nameCell.innerHTML = (isFirstRow ? '' : '- ') + name;
-      row.append(nameCell);
-
-      const countCell = document.createElement('td');
-      countCell.style.textAlign = 'center';
-      countCell.innerHTML = count;
-      row.append(countCell);
-
-      if (houndredPercentCount > 0) {
-        const percentCell = document.createElement('td');
-        const percent = Math.floor(100 * count/houndredPercentCount * 100) / 100;
-        percentCell.style.textAlign = 'center';
-        percentCell.innerHTML = '' + percent + '%';
-        row.append(percentCell);
-      }
-
-      tableElement.append(row);
-
-      isFirstRow = false;
-    }
-  }
-
-
-
-
-
-
-
-
-
-
-
-//-----------------------------
-
 });
 
 function addNewChallenge() {
