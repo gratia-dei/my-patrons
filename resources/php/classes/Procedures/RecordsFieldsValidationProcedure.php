324fb5dda2e87844ceb6c3f44c872846b0c4834b
Andrzej Precz <andrzej.precz@gmail.com>
2026-03-20 09:23:50 +0100

    record files validation - working step 1

--- resources/php/classes/Procedures/RecordsFieldsValidationProcedure.php
+++ resources/php/classes/Procedures/RecordsFieldsValidationProcedure.php
@@ -14,32 +14,73 @@ class RecordsFieldsValidationProcedure extends Procedure
             }
 
             $generatedFileSuffix = $this->getGeneratedFileSuffix();
+            $fullRootPath = $this->getFullDataPath($srcRootPath);
+            $fullPath = "$fullRootPath/$recordType";
 
-            $fullPath = $this->getFullDataPath("$srcRootPath/$recordType");
             $paths = $this->getPathTree($fullPath);
-
-            foreach ($paths as $path => $isDirectory) {
-                $dirName = dirname($path);
+            foreach ($paths as $staticFilePath => $isDirectory) {
+                $dirName = dirname($staticFilePath);
 
                 if ($isDirectory
-                    || in_array($path, [
+                    || in_array($staticFilePath, [
                         $this->getIndexFilePath($dirName),
                         $this->getIndexFilePath($dirName, true),
                         $this->getAliasFilePath($dirName),
                         $this->getAliasFilePath($dirName, true),
                     ])
-                    || false !== strpos($path, $generatedFileSuffix)
+                    || false !== strpos($staticFilePath, $generatedFileSuffix)
                 ) {
                     continue;
                 }
 
-                $this->validateFile($path, $pathData);
+                $extensionDotPosition = strrpos($staticFilePath, '.');
+                $staticFilePathWithoutExtension = mb_substr($staticFilePath, 0, $extensionDotPosition);
+                $generatedFilePath = $this->getGeneratedFileSuffix($staticFilePathWithoutExtension);
+
+                $staticData = $this->getOriginalJsonFileContentArrayForFullPath($staticFilePath);
+                $generatedData = $this->getOriginalJsonFileContentArrayForFullPath($generatedFilePath)[self::FIELDS_INDEX] ?? [];
+
+                $shortFilePath = mb_substr($staticFilePathWithoutExtension, mb_strlen($fullRootPath) + 1);
+
+                $this->validateFile($shortFilePath, $staticData, $generatedData, $pathData);
             }
         }
     }
 
-    private function validateFile(string $filePath, array $config): void
+    private function validateFile(string $sourceFilePath, array $staticData, array $generatedData, array $configData): void
     {
-        //todo
+        $configFields = $this->getConfigFields($configData);
+
+        $this->validate($sourceFilePath, $configFields, $staticData);
+    }
+
+    private function getConfigFields(array $configData): array
+    {
+        $result = [];
+
+        foreach ($configData as $fieldName => $fieldConfig) {
+            $fieldPath = explode('/', $fieldName);
+
+            $target = &$result;
+            foreach ($fieldPath as $element) {
+                if (!isset($target[$element])) {
+                    $target[$element] = [];
+                }
+                $target = &$target[$element];
+            }
+        }
+
+        return $result;
+    }
+
+    private function validate(string $filePath, array $configFieldsContext, mixed $staticDataContext, string $fieldPath = ''): void
+    {
+        foreach ($configFieldsContext as $subfield => $subFields) {
+            if (!array_key_exists($subfield, $staticDataContext)) {
+                $this->error("Missing field path '$fieldPath/$subfield' in source file '$filePath'");
+            }
+            $this->validate($filePath, $configFieldsContext[$subfield], $staticDataContext[$subfield] ?? null, $fieldPath . '/' . $subfield);
+            unset($staticDataContext[$subfield]);
+        }
     }
 }
